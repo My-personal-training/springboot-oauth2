00c7973569bedaf1cdbd3530dc6a5ef78ed670c8
Lorenzo Arias Villegas <santyago.arias.sav@gmail.com>
2024-05-18 19:56:21 -0400

    Setting up the project

---
+++ src/main/java/com/securitysandbox/oauth2/socialmediaaouth/SocialmediaaouthApplication.java
@@ -0,0 +1,13 @@
+package com.securitysandbox.oauth2.socialmediaaouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaaouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaaouthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securitysandbox/oauth2/socialmediaaouth/SocialmediaaouthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitysandbox.oauth2.socialmediaaouth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaaouthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
